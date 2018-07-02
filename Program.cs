a7685e2a821ab0a4fae63704158730086100a37e
SlimShaggy <slimshaggy@live.com>
2018-07-02 18:51:35 +0300

    Send 5000 messages only

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
-﻿using NLog;
+﻿using System;
+using NLog;
 using System.Threading;
 
 namespace SyslogTest
 {
     class Program
     {
+        private static int messageCount = 5000;
+
         static void Main(string[] args)
         {
             var nlog = LogManager.GetCurrentClassLogger();
+            int i = 1;
             while (true)
             {
-                nlog.Info("Test log message");
+                if (i <= messageCount)
+                {
+                    Console.WriteLine($"Sending message #{i}");
+                    nlog.Info("Test log message");
+                    i++;
+                }
                 Thread.Sleep(10);
             }
         }
