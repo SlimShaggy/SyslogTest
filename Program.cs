6652132b992e8e2170bb5300edf13ca7916f8f03
SlimShaggy <slimshaggy@live.com>
2018-07-02 18:41:55 +0300

    First version (sends messages in endless loop)

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using NLog;
+using System.Threading;
+
+namespace SyslogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nlog = LogManager.GetCurrentClassLogger();
+            while (true)
+            {
+                nlog.Info("Test log message");
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
